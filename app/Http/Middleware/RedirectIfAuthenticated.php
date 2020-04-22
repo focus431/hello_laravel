0a3c0899dd09a914fbefe43c170ed8117755af54
kevin431 <focus431@hotmail.com>
2020-04-22 22:51:23 +0800

    查看用户列表

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,8 +19,9 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            session()->flash('info', '您已登录，无需再次操作。');
-            return redirect('/');
+            return redirect(RouteServiceProvider::HOME);
         }
+
+        return $next($request);
     }
 }
