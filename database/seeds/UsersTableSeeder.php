24da79d9cc598e82794c008279942da889a46288
kevin431 <focus431@hotmail.com>
2020-04-24 09:37:18 +0800

    new password auth

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -16,7 +16,7 @@ class UsersTableSeeder extends Seeder
 
         $user = User::find(1);
         $user->name = 'Summer';
-        $user->email = 'summer@hotmail.com';
+        $user->email ='focus431@hotmail.com';
         $user->is_admin = true;
         $user->save();
     }
